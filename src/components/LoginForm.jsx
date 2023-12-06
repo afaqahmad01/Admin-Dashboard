240438265bfbe838ed0201e4d9e731ce8d8e2a7d
Afaq Ahmad <afaq01ahmad@gmail.com>
2023-12-07 00:59:27 +0500

    Login Page: Form to add username and password and button directed to dashboard page Dashboard Page Contains: 1.navbar contains 2 pages one for profile directed to profile page and one for logout to redirect to login page 2.1 card for users 3.1 card for admin posts Profile Page: 1.navbar contains 2 pages one for dashboard directed to dashboard page and one for logout to redirect to login page 2.admin details card

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,101 @@
+// LoginForm.js
+
+import React, { useState } from 'react';
+import ForgetPassword from "./ForgetPassword"
+import { Link } from 'react-router-dom';
+
+
+const Container = ({ children }) => (
+  <div className="flex justify-center flex-col gap-y-5 items-center h-screen">{children}</div>
+);
+
+const FormContainer = ({ children }) => (
+  <div className="w-96 p-6 bg-white rounded shadow-md">{children}</div>
+);
+
+const Title = ({ children }) => <h2 className="text-center mb-6">{children}</h2>;
+
+const Input = ({ type, placeholder, value, onChange }) => (
+  <input
+    type={type}
+    placeholder={placeholder}
+    value={value}
+    onChange={onChange}
+    className="w-full p-3 mb-4 border rounded"
+  />
+);
+
+const Button = ({ onClick, disabled, children }) => (
+  <button
+    onClick={onClick}
+    disabled={disabled}
+    className={`w-full p-3 bg-green-500 text-white border-none rounded cursor-pointer ${
+      disabled ? 'opacity-50 cursor-not-allowed' : ''
+    }`}
+  >
+    {children}
+  </button>
+);
+
+const LinkButton = ({ onClick, children }) => (
+  <button
+    onClick={onClick}
+    className="block mt-4 ml-[180px] w-44 text-blue-500 cursor-pointer hover:underline"
+  >
+    {children}
+  </button>
+);
+
+const handleForget=()=>{
+  <Link to="/forget-password" />
+}
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if (username && password) {
+      console.log('Username:', username);
+      console.log('Password:', password);
+    } else {
+      alert('Please fill in both username and password.');
+    }
+  };
+
+  return (
+    <div className='bg-[#40A858]'>
+      <Container>
+        <img src="https://i.imgur.com/ol5lsnc_d.jpg?maxwidth=520&shape=thumb&fidelity=high" alt="logo" />
+        <FormContainer>
+          <Title>Login</Title>
+          <Input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onClick={handleLogin} disabled={!username || !password}>
+          <Link to="/dashboard">
+          
+            Login
+            </Link>
+          </Button>
+          {/* <LinkButton>
+          <Link to="/forget-password">
+            Forgot Password?
+            </Link>
+            </LinkButton> */}
+        </FormContainer>
+      </Container>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,59 @@
+// src/components/Profile.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Profile = () => {
+  // Dummy data for admin details
+  const adminDetails = {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    role: 'Administrator',
+    joinedSince: 'Joined on January 1, 2022',
+  };
+
+  return (
+    <div>
+      {/* Navbar */}
+      <main className="flex-1 p-8">
+      <nav className="bg-green-500 p-4 mb-8">
+        <div className="flex justify-between items-center">
+          <div className="text-white font-bold text-xl">Your Website Name</div>
+          <div>
+            <Link to="/dashboard" className="text-white">
+              Dashboard
+            </Link>
+            <span> / </span>
+            <Link to="/" className="text-white">
+              Logout
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      {/* Profile Content */}
+      <div className="p-8">
+        <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
+
+        {/* Admin Details */}
+        <div className="bg-white p-6 rounded-lg shadow-md mb-4">
+          <h2 className="text-xl font-bold mb-2">Admin Details</h2>
+          <p>
+            <strong>Name:</strong> {adminDetails.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {adminDetails.email}
+          </p>
+          <p>
+            <strong>Role:</strong> {adminDetails.role}
+          </p>
+          <p>
+            <strong>{adminDetails.joinedSince}</strong>
+          </p>
+        </div>
+      </div>
+      </main>
+    </div>
+  );
+};
+
+export default Profile;
