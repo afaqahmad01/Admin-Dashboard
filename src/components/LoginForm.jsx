30ab39bb5788cddc61651bf7954a91254830c158
Afaq Ahmad <105989774+afaqahmad01@users.noreply.github.com>
2023-12-12 00:26:21 +0500

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -48,36 +48,6 @@ const LoginForm = () => {
             Login
           </button>
         </Link>
-        <img src="https://i.imgur.com/ol5lsnc_d.jpg?maxwidth=520&shape=thumb&fidelity=high" alt="logo" />
-        <div className="w-96 p-6 bg-white rounded shadow-md">
-          <h2 className="text-center mb-6">Login</h2>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full p-3 mb-4 border rounded"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-3 mb-4 border rounded"
-          />
-          <Link to="/dashboard">
-
-            <button
-              onClick={handleLogin}
-              disabled={!username || !password}
-              className={`w-full p-3 bg-green-500 text-white border-none rounded cursor-pointer ${
-                !username || !password ? 'opacity-50 cursor-not-allowed' : ''
-              }`}
-            >
-              Login
-            </button>
-          </Link>
-
 
         {/* <button
             className="block mt-4 ml-[180px] w-44 text-blue-500 cursor-pointer hover:underline"
@@ -85,14 +55,6 @@ const LoginForm = () => {
               window.location.href = '/forget-password';
             }}
           >
-          <Button onClick={handleLogin} disabled={!username || !password}>
-          
-            Login
-            
-          </Button>
-          </Link>
-            {/* <LinkButton>
-          <Link to="/forget-password">
             Forgot Password?
           </button> */}
       </div>
