200e9b3301ece484dc0c7db13fd6604f43a91328
Afaq Ahmad <105989774+afaqahmad01@users.noreply.github.com>
2023-12-12 00:24:29 +0500

    Update LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -17,37 +17,6 @@ const LoginForm = () => {
 
   return (
     <div className="flex justify-center flex-col gap-y-5 items-center h-screen bg-[#40A858]">
-      <img
-        src="https://i.imgur.com/ol5lsnc_d.jpg?maxwidth=520&shape=thumb&fidelity=high"
-        alt="logo"
-      />
-      <div className="w-96 p-6 bg-white rounded shadow-md">
-        <h2 className="text-center mb-6">Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="w-full p-3 mb-4 border rounded"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-3 mb-4 border rounded"
-        />
-        <Link to="/dashboard">
-          <button
-            onClick={handleLogin}
-            disabled={!username || !password}
-            className={`w-full p-3 bg-green-500 text-white border-none rounded cursor-pointer ${
-              !username || !password ? "opacity-50 cursor-not-allowed" : ""
-            }`}
-          >
-            Login
-          </button>
-        </Link>
         <img src="https://i.imgur.com/ol5lsnc_d.jpg?maxwidth=520&shape=thumb&fidelity=high" alt="logo" />
         <div className="w-96 p-6 bg-white rounded shadow-md">
           <h2 className="text-center mb-6">Login</h2>
