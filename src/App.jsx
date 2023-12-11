600a2edb683dba60da48557f785c007722b3cb19
Afaq Ahmad <afaq01ahmad@gmail.com>
2023-12-12 00:02:35 +0500

    User Card Button to access the single user detail page Make the full user details page: User Details Button to block/activate account of user Button of User Posts which on click shows: Post (Image/Video) Button if more than one post Button to remove post Reminder Clock which added by user Likes Comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginForm from './components/LoginForm';
-import ForgetPassword from './components/ForgetPassword';
-import Profile from './components/Profile';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import ForgetPassword from "./components/ForgetPassword";
+import Profile from "./components/Profile";
+import Dashboard from "./components/Dashboard";
+import UserDetails from "./components/UserDetails";
 
 const App = () => {
   return (
@@ -12,6 +13,8 @@ const App = () => {
         <Route path="/" exact element={<LoginForm />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/user/:userId" element={<UserDetails />} />{" "}
+        {/* Route for UserDetails */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// src/components/Dashboard.js
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const users = [
-    { username: 'user1', email: 'user1@example.com', posts: 3, status: 'Active' },
-    { username: 'user2', email: 'user2@example.com', posts: 5, status: 'Blocked' },
-    { username: 'user3', email: 'user3@example.com', posts: 2, status: 'Active' },
+    {
+      id: 1,
+      username: "user1",
+      email: "user1@example.com",
+      posts: 3,
+      status: "Active",
+    },
+    {
+      id: 2,
+      username: "user2",
+      email: "user2@example.com",
+      posts: 5,
+      status: "Blocked",
+    },
+    {
+      id: 3,
+      username: "user3",
+      email: "user3@example.com",
+      posts: 2,
+      status: "Active",
+    },
   ];
-
   const adminPosts = [
-    { postType: 'Image', postTag: 'Relaxing', uploadedSince: '2 days ago', likes: 15, comments: 3 },
-    { postType: 'Video', postTag: 'Motivational', uploadedSince: '1 week ago', likes: 20, comments: 5 },
-    { postType: 'Video', postTag: 'Happy', uploadedSince: '1 month and 2 days ago', likes: 172, comments: 52 },
-
+    {
+      id: 1,
+      postType: "Image",
+      postTag: "Relaxing",
+      uploadedSince: "2 days ago",
+      likes: 15,
+      comments: 3,
+    },
+    {
+      id: 2,
+      postType: "Video",
+      postTag: "Motivational",
+      uploadedSince: "1 week ago",
+      likes: 20,
+      comments: 5,
+    },
+    {
+      id: 3,
+      postType: "Video",
+      postTag: "Happy",
+      uploadedSince: "1 month and 2 days ago",
+      likes: 172,
+      comments: 52,
+    },
   ];
 
   return (
@@ -50,11 +88,30 @@ const Dashboard = () => {
                 <tbody>
                   {users.map((user, index) => (
                     <tr key={user.id}>
-                      <td className="border border-gray-300 p-2">{index + 1}</td>
-                      <td className="border border-gray-300 p-2">{user.username}</td>
-                      <td className="border border-gray-300 p-2">{user.email}</td>
-                      <td className="border border-gray-300 p-2">{user.posts}</td>
-                      <td className={`border border-gray-300 p-2 ${user.status === 'Blocked' ? 'text-red-500' : 'text-green-500'}`}>
+                      <td className="border border-gray-300 p-2">
+                        {index + 1}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        <Link
+                          to={`/user/${user.id}`}
+                          className="text-blue-500 hover:underline"
+                        >
+                          {user.username}
+                        </Link>
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {user.email}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {user.posts}
+                      </td>
+                      <td
+                        className={`border border-gray-300 p-2 ${
+                          user.status === "Blocked"
+                            ? "text-red-500"
+                            : "text-green-500"
+                        }`}
+                      >
                         {user.status}
                       </td>
                     </tr>
@@ -73,7 +130,9 @@ const Dashboard = () => {
                     <th className="border border-gray-300 p-2">#</th>
                     <th className="border border-gray-300 p-2">Post Type</th>
                     <th className="border border-gray-300 p-2">Post Tag</th>
-                    <th className="border border-gray-300 p-2">Uploaded Since</th>
+                    <th className="border border-gray-300 p-2">
+                      Uploaded Since
+                    </th>
                     <th className="border border-gray-300 p-2">Likes</th>
                     <th className="border border-gray-300 p-2">Comments</th>
                   </tr>
@@ -81,12 +140,24 @@ const Dashboard = () => {
                 <tbody>
                   {adminPosts.map((post, index) => (
                     <tr key={post.id}>
-                      <td className="border border-gray-300 p-2">{index + 1}</td>
-                      <td className="border border-gray-300 p-2">{post.postType}</td>
-                      <td className="border border-gray-300 p-2">{post.postTag}</td>
-                      <td className="border border-gray-300 p-2">{post.uploadedSince}</td>
-                      <td className="border border-gray-300 p-2">{post.likes}</td>
-                      <td className="border border-gray-300 p-2">{post.comments}</td>
+                      <td className="border border-gray-300 p-2">
+                        {index + 1}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {post.postType}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {post.postTag}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {post.uploadedSince}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {post.likes}
+                      </td>
+                      <td className="border border-gray-300 p-2">
+                        {post.comments}
+                      </td>
                     </tr>
                   ))}
                 </tbody>
